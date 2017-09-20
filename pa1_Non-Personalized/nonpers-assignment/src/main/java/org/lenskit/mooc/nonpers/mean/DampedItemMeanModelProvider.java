a4ace00b266b7324522951d8c7246ae18f969e1e
chen4626 <chen4626@umn.edu>
2017-09-19 20:29:09 -0500

    add: Computing Damped Items Means

--- pa1_Non-Personalized/nonpers-assignment/src/main/java/org/lenskit/mooc/nonpers/mean/DampedItemMeanModelProvider.java
+++ pa1_Non-Personalized/nonpers-assignment/src/main/java/org/lenskit/mooc/nonpers/mean/DampedItemMeanModelProvider.java
@@ -1,7 +1,6 @@
 package org.lenskit.mooc.nonpers.mean;
 
 import it.unimi.dsi.fastutil.longs.Long2DoubleOpenHashMap;
-import it.unimi.dsi.fastutil.longs.Long2IntOpenHashMap;
 import org.lenskit.baseline.MeanDamping;
 import org.lenskit.data.dao.DataAccessObject;
 import org.lenskit.data.ratings.Rating;
@@ -12,6 +11,10 @@ import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Provider class that builds the mean rating item scorer, computing damped item means from the
@@ -62,7 +65,45 @@ public class DampedItemMeanModelProvider implements Provider<ItemMeanModel> {
     @Override
     public ItemMeanModel get() {
         // TODO Compute damped means
-        // TODO Remove the line below when you have finished
-        throw new UnsupportedOperationException("damped mean not implemented");
+        double globalSum = 0;
+        int globalCount = 0;
+        // TODO Set up data structures for computing means
+        Map<Long, List<Double>> map = new HashMap<>();
+
+        try (ObjectStream<Rating> ratings = dao.query(Rating.class).stream()) {
+            for (Rating r : ratings) {
+                // this loop will run once for each rating in the data set
+                // TODO process this rating
+                if (!map.containsKey(r.getItemId())) {
+                    map.put(r.getItemId(), new ArrayList<Double>());
+                }
+                map.get(r.getItemId()).add(r.getValue());
+                globalSum += r.getValue();
+                globalCount += 1;
+            }
+        } catch (Exception e) {
+            logger.error("error in query");
+//            throw UnsupportedOperationException e;
+        }
+        logger.info("get global sum {}", globalSum);
+
+
+        double globalMeanRating = globalSum / globalCount;
+
+        Long2DoubleOpenHashMap dampedMeans = new Long2DoubleOpenHashMap();
+        // TODO Finalize means to store them in the mean model
+        for (Long itemId : map.keySet()) {
+            double sum = 0;
+            for (Double rating : map.get(itemId)) {
+                sum += rating;
+            }
+            double avg = (sum + this.damping * globalMeanRating) / (map.get(itemId).size() + this.damping);
+            dampedMeans.put(itemId, (Double) avg);
+        }
+        logger.info("get means {}", dampedMeans.get(2959));
+        logger.info("get means {}", dampedMeans.get(1203));
+        logger.info("computed mean ratings for {} items", dampedMeans.size());
+
+        return new ItemMeanModel(dampedMeans);
     }
 }
